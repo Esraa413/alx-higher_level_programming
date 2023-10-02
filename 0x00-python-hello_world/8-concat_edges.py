aedc79a5846b929751b5eafbf2d5137a50881f36
Esraa413 <esraaabdelwahab614@gmail.com>
2023-10-02 15:43:57 -0700

    con_edg

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-st = "object-oriented programming"
-st2 = "power with"
-st3 = " Python"
-str2 = st + st2 + st3
-print(str2)
+print(f"{str[39:67]}{str[107:112]}{str[:6]}")
