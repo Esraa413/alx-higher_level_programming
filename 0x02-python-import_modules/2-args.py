40581477b31c9d6f2e46e5b3e4b551b687f84fc8
Esraa413 <esraaabdelwahab614@gmail.com>
2023-10-05 13:08:51 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if arge == 0:
         print("0 arguments.")
     elif arge == 1:
-        print("1 arguments:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(arge))
 
