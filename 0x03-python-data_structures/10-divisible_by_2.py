f7158ee6a31d1cb02be9cdd07933be6635b64e79
Esraa413 <esraaabdelwahab614@gmail.com>
2023-10-09 16:34:54 -0700

    list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     mult = []
     for x in range(len(my_list)):
-        if my_list[i] % 2 == 0:
+        if my_list[x] % 2 == 0:
             mult.append(True)
         else:
             mult.append(False)
