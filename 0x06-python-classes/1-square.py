526bc8b422feffd317c2162ab4afd6fe7ffc4b07
Esraa413 <esraaabdelwahab614@gmail.com>
2023-10-24 13:10:54 -0700

    class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
+
 """ class Square """
 
-
 class Square:
-""" class defines a square """
-
+    """ class defines a square """
 
 
     def __init__(self, size):
         """ Instantiation with size.
 
-        Arge:
-        size: (int) size of new  square.
-
+        Args:
+            size: (int) size of new square
         """
-
-
 
         self.__size = size
