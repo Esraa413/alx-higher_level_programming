5206230ee54e21fada20930c4a297c1c402b6174
Esraa413 <esraaabdelwahab614@gmail.com>
2023-10-24 15:50:31 -0700

    cless

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,7 +28,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-
+    @property
     def position(self):
         """current position of the square."""
         return (self.__position)
@@ -37,8 +37,8 @@ class Square:
     def position(self, value):
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+                not all(isinstance(number, int) for number in value) or
+                not all(number >= 0 for number in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
