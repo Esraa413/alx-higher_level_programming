2dab113df33a167483ae6322ff1bbc832c7678d0
Esraa413 <esraaabdelwahab614@gmail.com>
2023-10-24 15:55:55 -0700

    cless

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -37,23 +37,23 @@ class Square:
     def position(self, value):
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+                not all(isinstance(number, int) for number in value) or
+                not all(number >= 0 for number in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-        def area(self):
-            """returns the current square area."""
-            return (self.__size * self.__size)
+    def area(self):
+        """returns the current square area."""
+        return (self.__size * self.__size)
+    
+    def my_print(self):
+        """Print the square"""
+        if self.__size == 0:
+            print("")
+            return
 
-        def my_print(self):
-            """Print the square"""
-            if self.__size == 0:
-                print("")
-                return
-
-            [print("") for x in range(0, self.__position[1])]
-            for x in range(0, self.__size):
-                [print(" ", end="") for y in range(0, self.__position[0])]
-                [print("#", end="") for i in range(0, self.__size)]
-                print("")
+        [print("") for x in range(0, self.__position[1])]
+        for x in range(0, self.__size):
+            [print(" ", end="") for y in range(0, self.__position[0])]
+            [print("#", end="") for i in range(0, self.__size)]
+            print("")
