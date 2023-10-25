d3965c5afeadb55049ac9fe7a9c07a3e2095f992
Esraa413 <esraaabdelwahab614@gmail.com>
2023-10-25 11:51:56 -0700

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    """Print x of list
+
+    Args:
+        my_list (list): print elements.
+        x (int): number of elements to my_list.
+
+    Return:
+        number of elements.
+    """
+
     new = 0
     for y in range(x):
         try:
