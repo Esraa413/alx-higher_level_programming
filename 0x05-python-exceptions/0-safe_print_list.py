a05a60b234ff67fee55ea5614dc4b4419439ab77
Esraa413 <esraaabdelwahab614@gmail.com>
2023-10-25 11:53:03 -0700

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -14,7 +14,7 @@ def safe_print_list(my_list=[], x=0):
     for y in range(x):
         try:
             print("{}".format(my_list[y]), end="")
-            new = +1
+            new += 1
         except IndexError:
             break
     print("")
