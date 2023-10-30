cbb9ef0b8f37f0b4d9b7202e0a327326549df46f
Esraa413 <esraaabdelwahab614@gmail.com>
2023-10-30 14:41:23 -0700

    class

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""empty Rectangle class using the pass keyword"""
+
+
+class Rectangle:
+    """Represent defines a rectangle.
+
+    Args:
+        width (int): the width
+        height (int): the height
+    """
