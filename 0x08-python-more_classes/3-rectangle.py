431ae9fc8d7643e51a740c1744073a0fe7c9d870
Esraa413 <esraaabdelwahab614@gmail.com>
2023-10-30 14:10:49 -0700

    class

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+"""empty Rectangle class using the pass keyword"""
+
+
+class Rectangle:
+    """Represent defines a rectangle.
+
+    Args:
+        width (int): the width
+        height (int): the height
+    """
+    def __init__(self, width=0, height=0):
+        """optional width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """to retrieve it"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width must be an integer, otherwise raise a TypeError"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """instance attribute: height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ must be an integer, otherwise raise a TypeError"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Public instance method"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Public instance method"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (2 * self.__width) + (2 * self.height)
+
+    def __str__(self):
+       """printable representation of the Rectangle"""
+       if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rect = []
+        for x in range(self.__height):
+            [rect.append('#') for y in range(self.__width)]
+            if x != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
