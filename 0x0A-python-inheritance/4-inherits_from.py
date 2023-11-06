3baa9837d0cf7dc9e025a09ba44a122209c506f1
Esraa413 <esraaabdelwahab614@gmail.com>
2023-11-06 14:49:40 -0800

    python

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-""" if the object is an instance of a class that inherited """
+""" if the object is an instance of a class that inherited."""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of,
+    or if the object is an instance of,
+    a class that inherited from, the specified clas otherwise False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class BaseGeometry."""
+
+
+class BaseGeometry:
+    """lass BaseGeometry (based on 5-base_geometry.py)."""
+
+    def area(self):
+        """Public instance method
+        """
+        raise Exception("area() is not implemented")
