c97f1e3c90cc942eb8ffa24faf27788fabf1c223
Esraa413 <esraaabdelwahab614@gmail.com>
2023-11-06 15:49:59 -0800

    python

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,19 +4,20 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """from BaseGeometry(7-base_geometry.py).(task based on 8-rectangle.py)."""
+    """from BaseGeometry(7-base_geometry.py).(task based)."""
 
-     def __init__(self, width, height):
+    def __init__(self, width, height):
          """Instantiation with width and height.
 
          Args:
             width: of the new Rectangle.
             height: of the new Rectangle.
-         """
-         super().integer_validator("width", width)
-         self.__width = width
-         super().integer_validator("height", height)
-         self.__height = height
+        """
+
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
 
     def area(self):
         """Return the area."""
@@ -27,3 +28,4 @@ class Rectangle(BaseGeometry):
         string = "[" + str(self.__class__.__name__) + "] "
         string += str(self.__width) + "/" + str(self.__height)
         return string
+
