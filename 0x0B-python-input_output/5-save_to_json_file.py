6b9f1f9e93a80b418e55a0a9a865373adcf5a800
Esraa413 <esraaabdelwahab614@gmail.com>
2023-11-07 15:09:13 -0800

    python

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,7 +3,8 @@
 import json
 
 
-def load_from_json_file(filename):
-    """writes an Object to a text file, using a JSON representation."""
-    with open(filename) as f:
-        return json.load(f)
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file using a JSON representation."""
+     with open(filename, "w") as f:
+        json.dump(my_obj, f)
+    
