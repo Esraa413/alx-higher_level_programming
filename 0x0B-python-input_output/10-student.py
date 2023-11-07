f9c48e667b7d05394048f1f8cfd6c8a9a793f7d0
Esraa413 <esraaabdelwahab614@gmail.com>
2023-11-07 14:05:32 -0800

    python

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -2,7 +2,6 @@
 """function class Student."""
 
 
-
 class Student:
     """student epresent."""
 
@@ -17,6 +16,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
 
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
