5253e7c8843d23e30111b429436951a01beeb68d
Esraa413 <esraaabdelwahab614@gmail.com>
2023-11-07 14:13:09 -0800

    python

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""function class Student."""
+"""class Student that defines a student"""
 
 
 class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize Student.
+        """Initialize  Student.
 
         Args:
-            first_name (str): student first name 
+            first_name (str): student first name
             last_name (str): student last name
             age (int): The age student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self, attrs=None):
         """Get a dictionary representation.
