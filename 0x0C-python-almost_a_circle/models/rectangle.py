d80c99a58878f88def2f16b5612980cb0de148a1
Esraa413 <esraaabdelwahab614@gmail.com>
2023-11-13 13:15:56 -0800

    circal_almost

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Return area Rectangle."""
+        return self.width * self.height
