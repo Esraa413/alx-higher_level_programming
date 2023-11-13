f608129abbc4606b2dcc378969cbc83d54b8a9eb
Esraa413 <esraaabdelwahab614@gmail.com>
2023-11-13 14:53:07 -0800

    circal_almost

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,14 +21,22 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return JSON serialization list of dicts."""
+        """Return the JSON serialization of a list of dicts.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Write JSON serialization list objects to file."""
+        """Write the JSON serialization of a list of objects to a file.
+
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
         filename = cls.__name__ + ".json"
         with open(filename, "w") as jsonfile:
             if list_objs is None:
