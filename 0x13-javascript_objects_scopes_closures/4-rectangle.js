1d77fbf80ef321489e2af3f9b23efeb8ad5aa9b9
Esraa413 <esraaabdelwahab614@gmail.com>
2024-01-09 23:41:00 +0000

    object

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,29 @@
-hgfg
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+print () {
+    for (let j = 0; j < this.height; j++) {
+      let v = '';
+      for (let t = 0; t < this.width; t++) {
+        v += 'X';
+      }
+      console.log(v);
+    }
+  }
+}
+rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
