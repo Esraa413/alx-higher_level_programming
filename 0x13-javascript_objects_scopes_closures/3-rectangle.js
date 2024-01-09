58b48ccde9610ba6696153b279616f70139206ec
Esraa413 <esraaabdelwahab614@gmail.com>
2024-01-09 23:42:34 +0000

    object

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,7 +6,7 @@ class Rectangle {
       this.height = h;
     }
   }
-print () {
+  print () {
     for (let j = 0; j < this.height; j++) {
       let v = '';
       for (let t = 0; t < this.width; t++) {
